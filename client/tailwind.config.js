4b3ca14b843e0e1adbf4f97a9029852c1aa9672d
Youngspud <waynesperk82@gmail.com>
2026-04-24 16:27:23 +0300

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",   // 👈 THIS enables manual dark mode
+
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {},
   },
+
   plugins: [],
 }
