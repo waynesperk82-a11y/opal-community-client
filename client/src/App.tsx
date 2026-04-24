b466b8284b9f7a7dec0dd296a1d293fd73f18ea5
Youngspud <waynesperk82@gmail.com>
2026-04-24 16:17:42 +0300

    Remove unnecessary blank line in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
-
 import Home from "./pages/Home";
 import Ask from "./pages/Ask";
 import About from "./pages/About";
