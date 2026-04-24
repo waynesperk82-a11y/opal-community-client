bb0d3f255b5a61b72447ae304a58ae2ca4f84a0b
Youngspud <waynesperk82@gmail.com>
2026-04-24 16:08:40 +0300

    Update Sidebar links and icons

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -3,30 +3,32 @@ import { Link } from "react-router-dom";
 export default function Sidebar() {
   return (
     <aside className="w-64 hidden md:block p-6 border-r bg-white min-h-screen">
+
       <div className="space-y-4">
 
         <Link
           to="/"
           className="block px-4 py-2 rounded-lg hover:bg-gray-100"
         >
-          Home
+          🏠 Home
         </Link>
 
         <Link
           to="/ask"
           className="block px-4 py-2 rounded-lg hover:bg-gray-100"
         >
-          Ask Question
+          ❓ Ask Question
         </Link>
 
         <Link
-          to="/login"
+          to="/about"
           className="block px-4 py-2 rounded-lg hover:bg-gray-100"
         >
-          Login
+          ℹ About
         </Link>
 
       </div>
+
     </aside>
   );
 }
