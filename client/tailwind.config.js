aa0130c8defb3283d7d88d0e15e2765198b61a20
Youngspud <waynesperk82@gmail.com>
2026-04-24 16:43:26 +0300

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",   // 👈 THIS enables manual dark mode
-
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        zoomIn: "zoomIn 0.3s ease-out forwards",
+      },
+      keyframes: {
+        zoomIn: {
+          "0%": {
+            opacity: "0",
+            transform: "scale(0.8)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "scale(1)",
+          },
+        },
+      },
+    },
   },
-
   plugins: [],
 }
